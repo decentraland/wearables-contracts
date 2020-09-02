0b17d2ee34131d0bf02f02f7d022d057829b5341
Ignacio Mazzara <nachomazzara@gmail.com>
2020-09-02 09:58:48 -0300

    feat: remove unused code

--- contracts/interfaces/IERC721CollectionV2.sol
+++ contracts/interfaces/IERC721CollectionV2.sol
@@ -24,15 +24,8 @@ interface IERC721CollectionV2 {
         string metadata;
     }
 
-    // function issueToken(address _beneficiary, string calldata _wearableId) external;
-    // function getWearableKey(string calldata _wearableId) external view returns (bytes32);
-    // function issued(bytes32 _wearableKey) external view returns (uint256);
-    // function maxIssuance(bytes32 _wearableKey) external view returns (uint256);
-    // function owner() external view returns (address);
-    // function wearables(uint256 _index) external view returns (string memory);
     function issueToken(address _beneficiary, uint256 _itemId) external;
-    function issueTokens(address[] calldata  _beneficiaries, uint256[] calldata _itemIds) external;
-   // function items(uint256 _itemId) external view returns (Item memory);
     function items(uint256 _itemId) external view returns (uint256, uint256, uint256, address, string memory, bytes32);
+    // function items(uint256 _itemId) external view returns (Item memory);
 
 }
