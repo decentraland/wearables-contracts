b30db878062b23d5d0f21df1df869b60e640e845
Ignacio Mazzara <nachomazzara@gmail.com>
2021-02-04 12:28:08 -0300

    fix: config

--- utils/getDeployParams.ts
+++ utils/getDeployParams.ts
@@ -2,11 +2,11 @@ require('dotenv').config()
 
 export function getDeployParams() {
   return {
-    url: process.env[`RPC_URL_${process.env['NETWORK']}`] || '',
+    url: process.env[`RPC_URL_${process.env['NETWORK']}`] || 'https://rinkeby.infura.io/v3/',
     accounts: {
-      mnemonic: process.env['MNEMONIC']?.replace(/,/g, ' '),
+      mnemonic: process.env['MNEMONIC'] ? process.env['MNEMONIC'].replace(/,/g, ' ') : 'test test test test test test test test test test test junk',
       initialIndex: 0,
-      count: 1,
+      count: 10,
       path: `m/44'/60'/0'/0`
     },
     gas: "auto",
