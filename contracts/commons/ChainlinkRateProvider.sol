a573bd5bf9da49b1c6b9ab1ae6c7bf43bca243e8
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2022-01-17 17:42:42 -0300

    feat: Rename ChainlinkRateProvider and get returned decimals from aggregator interface

---
+++ contracts/commons/ChainlinkRateProvider.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.3;
+pragma experimental ABIEncoderV2;
+
+import '@openzeppelin/contracts/math/SafeMath.sol';
+
+import '../interfaces/IRateProvider.sol';
+import '../interfaces/chainlink/AggregatorV3Interface.sol';
+
+contract ChainlinkRateProvider is IRateProvider {
+    using SafeMath for uint256;
+
+    AggregatorV3Interface public immutable rateFeed;
+    uint256 public immutable expectedDecimals;
+
+    constructor(AggregatorV3Interface _rateFeed, uint256 _expectedDecimals) {
+        rateFeed = _rateFeed;
+        expectedDecimals = _expectedDecimals;
+    }
+
+    /**
+     * @notice Get rate
+     * @return rate in the expected token decimals
+     */
+    function getRate() external view override returns (uint256) {
+        uint256 decimals = uint256(rateFeed.decimals());
+
+        (, int256 rate, , , ) = rateFeed.latestRoundData();
+
+        if (rate < 0) {
+            revert('ChainlinkRateProvider#getRate: RATE_BELOW_0');
+        }
+
+        return uint256(rate).mul(10**(decimals.sub(expectedDecimals)));
+    }
+}
