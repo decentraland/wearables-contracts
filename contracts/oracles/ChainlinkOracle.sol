232184443fa1d646cda7a638c71de556a9db8f85
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2022-01-18 12:34:04 -0300

    chore: Add constructor comment to chainlink oracle

--- contracts/oracles/ChainlinkOracle.sol
+++ contracts/oracles/ChainlinkOracle.sol
@@ -14,6 +14,11 @@ contract ChainlinkOracle is IOracle {
     AggregatorV3Interface public immutable rateFeed;
     uint256 public immutable decimals;
 
+    /**
+     * @notice Create the contract
+     * @param _rateFeed - chainlink's data feed address to obtain a rate. https://docs.chain.link/docs/get-the-latest-price/#solidity
+     * @param _decimals - amount of decimals the rate should be returned with
+     */
     constructor(AggregatorV3Interface _rateFeed, uint256 _decimals) {
         rateFeed = _rateFeed;
         decimals = _decimals;
