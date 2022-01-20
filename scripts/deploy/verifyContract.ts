8211007c5382e68e4f9e6a34ac0cdb5265924d8f
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2022-01-20 12:06:29 -0300

    chore: Recover verifyContract script

---
+++ scripts/deploy/verifyContract.ts
@@ -0,0 +1,10 @@
+async function main() {
+  // TODO: Add something to verify if necessary
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
