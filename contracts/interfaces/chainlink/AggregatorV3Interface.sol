dc5f8241ea14e1eed879547d264206890a7ae9e5
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2022-01-21 16:05:46 -0300

    chore: Add natspec to aggregator

--- contracts/interfaces/chainlink/AggregatorV3Interface.sol
+++ contracts/interfaces/chainlink/AggregatorV3Interface.sol
@@ -3,35 +3,70 @@
 pragma solidity ^0.7.6;
 pragma experimental ABIEncoderV2;
 
+/**
+ * @notice Interface used by Chainlink Data Feeds
+ * @dev Obtained from https://github.com/smartcontractkit/chainlink/blob/develop/contracts/src/v0.7/interfaces/AggregatorV3Interface.sol
+ *      Not using @chainlink/contracts because of the amount of unnecessary stuff it installs while only this interface is required
+ *      Natspec added from https://docs.chain.link/docs/price-feeds-api-reference/
+ *      getRoundData and latestRoundData should both raise "No data present"
+ *      if they do not have data to report, instead of returning unset values
+ *      which could be misinterpreted as actual reported values.
+ */
 interface AggregatorV3Interface {
-  function decimals() external view returns (uint8);
+    /**
+     * @notice Get the number of decimals present in the response value
+     * @return The number of decimals
+     */
+    function decimals() external view returns (uint8);
 
-  function description() external view returns (string memory);
+    /**
+     * @notice Get the description of the underlying aggregator that the proxy points to
+     * @return The description of the underlying aggregator
+     */
+    function description() external view returns (string memory);
 
-  function version() external view returns (uint256);
+    /**
+     * @notice The version representing the type of aggregator the proxy points to
+     * @return The version number
+     */
+    function version() external view returns (uint256);
 
-  // getRoundData and latestRoundData should both raise "No data present"
-  // if they do not have data to report, instead of returning unset values
-  // which could be misinterpreted as actual reported values.
-  function getRoundData(uint80 _roundId)
-    external
-    view
-    returns (
-      uint80 roundId,
-      int256 answer,
-      uint256 startedAt,
-      uint256 updatedAt,
-      uint80 answeredInRound
-    );
+    /**
+     * @notice Get data about a specific round, using the roundId
+     * @param _roundId - The round ID
+     * @return roundId - The round ID
+     * @return answer - The price 
+     * @return startedAt - Timestamp of when the round started
+     * @return updatedAt - Timestamp of when the round was updated
+     * @return answeredInRound - The round ID of the round in which the answer was computed
+     */
+    function getRoundData(uint80 _roundId)
+        external
+        view
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        );
 
-  function latestRoundData()
-    external
-    view
-    returns (
-      uint80 roundId,
-      int256 answer,
-      uint256 startedAt,
-      uint256 updatedAt,
-      uint80 answeredInRound
-    );
+    /**
+     * @notice Get the price from the latest round
+     * @return roundId - The round ID
+     * @return answer - The price 
+     * @return startedAt - Timestamp of when the round started
+     * @return updatedAt - Timestamp of when the round was updated
+     * @return answeredInRound - The round ID of the round in which the answer was computed
+     */
+    function latestRoundData()
+        external
+        view
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        );
 }
