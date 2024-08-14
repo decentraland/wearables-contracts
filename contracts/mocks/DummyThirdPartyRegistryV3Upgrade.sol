1d434ad696db790457a4fecd3c5dfef15c8f2429
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2024-08-14 12:33:31 -0300

    fix: Remove old event

--- contracts/mocks/DummyThirdPartyRegistryV3Upgrade.sol
+++ contracts/mocks/DummyThirdPartyRegistryV3Upgrade.sol
@@ -98,7 +98,7 @@ contract DummyThirdPartyRegistryV3Upgrade is OwnableInitializable, NativeMetaTra
 
     mapping(string => bool) public isThirdPartyProgrammatic;
 
-    event ThirdPartyAdded(string _thirdPartyId, string _metadata, string _resolver, bool _isApproved, address[] _managers, uint256 _itemSlots, address _sender);
+    event ThirdPartyAdded(string _thirdPartyId, string _metadata, string _resolver, bool _isApproved, address[] _managers, uint256 _itemSlots, address _sender, bool _isProgrammatic);
     event ThirdPartyUpdated(string _thirdPartyId, string _metadata, string _resolver, address[] _managers, bool[] _managerValues, uint256 _itemSlots, address _sender);
     event ThirdPartyItemSlotsBought(string _thirdPartyId, uint256 _price, uint256 _value, address _sender);
     event ThirdPartyReviewed(string _thirdPartyId, bool _value, address _sender);
--- contracts/registries/ThirdPartyRegistryV3.sol
+++ contracts/registries/ThirdPartyRegistryV3.sol
@@ -98,7 +98,6 @@ contract ThirdPartyRegistryV3 is OwnableInitializable, NativeMetaTransaction, In
 
     mapping(string => bool) public isThirdPartyProgrammatic;
 
-    event ThirdPartyAdded(string _thirdPartyId, string _metadata, string _resolver, bool _isApproved, address[] _managers, uint256 _itemSlots, address _sender);
     event ThirdPartyAdded(string _thirdPartyId, string _metadata, string _resolver, bool _isApproved, address[] _managers, uint256 _itemSlots, address _sender, bool _isProgrammatic);
     event ThirdPartyUpdated(string _thirdPartyId, string _metadata, string _resolver, address[] _managers, bool[] _managerValues, uint256 _itemSlots, address _sender);
     event ThirdPartyItemSlotsBought(string _thirdPartyId, uint256 _price, uint256 _value, address _sender);
